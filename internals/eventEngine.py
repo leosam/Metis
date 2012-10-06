ca87264487fdf46e88db7c19d060c78d83477b5e
Leonardo S. Cardoso <leosam@ganymede.(none)>
2012-10-06 03:16:54 +0200

    Started to clean up the code. Inserted some comments and did some formatting in EventEngine. Created the singleton mode for EventEngine to bypass the globalsManagers solution. Restructured metis.py.

--- internals/eventEngine.py
+++ internals/eventEngine.py
@@ -1,4 +1,3 @@
-#!/usr/bin/python
 import threading
 import os
 #import festival
@@ -13,57 +12,111 @@ import userModule
 import plugin_mgr
 
 class EventEngine(threading.Thread):
+   '''
+   The EventEngine Class is responsible for treating all incomming events
+   '''
+
    def __init__(self):
+      '''
+      Constructor
+      '''
+
       threading.Thread.__init__(self)
       self.eventqueue = Queue.Queue()
       self.finished = 0
       self.PluginManager = plugin_mgr.PluginManagerClass(self)
 
+
    def getPluginManager(self):
+      '''
+      Returns the PluginManager
+      '''
+
       return self.PluginManager
 
-   def post(self,task):
-      logging.debug('(in EventEngine) posting event %s' %(task.name))
-      self.eventqueue.put(task)
+
+   def post(self,event):
+      '''
+      Posts an event to the EventEngine's queue to be processed
+
+      @param task: The event
+      @type task: A task object
+      '''
+
+      self.eventqueue.put(event)
+
+      logging.debug('(in EventEngine) posting event %s' %(event.name))
+
 
    def stopengine(self):
-      logging.warning("SETTING TO STOP")
+      '''
+      Stops the engine
+      '''
+
       self.finished = 1
 
+      logging.warning("SETTING TO STOP")
+
+
    def run(self):
+      '''
+      Run the EventEngine
+      '''
+
       logging.warning("STARTING")
+      
       while(not self.finished):
-         newEvent = self.eventqueue.get() #python's bug: can't be killed by Ctrl+C
-         logging.debug("get event %s" %(newEvent.name))
+         
+         # Get the next event from the queue
+         nextEvent = self.eventqueue.get() #python's bug: can't be killed by Ctrl+C
+         logging.debug("get event %s" %(nextEvent.name))
+         
+         # TODO: clarify -> What are these newTasks?
          newTasks = list()
+         
+         # Get users that might be interested in this event
          for u in userModule.getUsers():
+            
             logging.warning("eventEngine sees user %s" %(u.name))
-            ep = u.getProfileByEvent(newEvent)
+            
+            ep = u.getProfileByEvent(nextEvent)
+            
             if (ep != None):
-               logging.debug("(eventEngine) user's %s profile for event %s has actions : " %(u.name, newEvent.name))
+               logging.debug("(eventEngine) user's %s profile for event %s has actions : " %(u.name, nextEvent.name))
                for a in ep.getActions():
                   logging.debug(a.name)
-               #TOTO: define policy
-               # should we execute each action for each user?
-               # some (at least internals) actions need to be executed only once... not on a per-user basis (typically user creation...)
+                  
+                  # TODO: define policy
+                  #   should we execute each action for each user?
+                  #   some (at least internals) actions need to be executed only once... 
+                  #   not on a per-user basis (typically user creation...)
+                  
                newTasks.extend(ep.getActions())
+
             else:
                logging.error("BEWARE! user %s has no EventProfile attached!!" %(u.name))
+
          newEvent.actionArgs.update({'testArg':"fromEngine"}) #optional, but the engine could add info on users or whatever state it wants and give that to the action
+         
          if (len(newTasks) <= 0):
-            logging.warning("no action to execute for event %s" %(newEvent.name))
+            logging.warning("no action to execute for event %s" %(nextEvent.name))
+         
          for t in newTasks:
-            logging.debug("eventEngine executing %s for event %s (args are %s)" %(t.name, newEvent.name, newEvent.actionArgs))
-            t(newEvent.actionArgs)
+            logging.debug("eventEngine executing %s for event %s (args are %s)" %(t.name, nextEvent.name, newEvent.actionArgs))
+            t(nextEvent.actionArgs)
             t.treated = 1
       
       time.sleep(5) #TODO: remove ugly sleep and actually wait on all plugins to stop instead
       logging.warning("STOPPING")
 
-def endtask(**args):
-   args['engine'].post(action_def.Action(args['engine'].stopengine))
 
-def dummytask(**args):
-   logging.warning("DUMMY TASK, args: %s" %(str(args)))
+# To be able to instantiate a sole EventEngine we need to do some dirty tricks:
+# Singleton methodology...
+class TheEventEngine(EventEngine):
 
+   __instance = None
 
+   def __new__(cls):
+      if cls.__instance == None:
+         cls.__instance = EventEngine.__new__(cls)
+      return cls.__instance
